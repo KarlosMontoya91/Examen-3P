9e0de72c127a704e741e650216e31dc278f0d376
KarlosMontoya91 <66183570+KarlosMontoya91@users.noreply.github.com>
2020-08-14 23:19:05 -0500

    Create Ejercicio3.js

---
+++ Ejercicio3.js
@@ -0,0 +1,16 @@
+function draw() {
+  background(220);
+  relacion(10,5)
+}
+
+function relacion(a,b){
+  if (a>b) {
+    console.log (1)
+  } 
+  if(a<b){
+    console.log (-1)
+  }  
+  if (a==b){
+    console.log (0)
+  }
+}
